06c51e5bf8b446e87938baf4f1f8c4dd4453d40f
Luc van Kerkvoort <luc.van.kerkvoort@gmail.com>
2020-06-06 08:09:36 -0700

    Updates

---
+++ precache-manifest.c6877324ce9c9c83819226940a5f7c99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c173952f826ec74f1aeb9395941c79fd",
+    "url": "/RoboBuddies/index.html"
+  },
+  {
+    "revision": "b1554304c156b50bd5ba",
+    "url": "/RoboBuddies/static/css/main.9d66d7e6.chunk.css"
+  },
+  {
+    "revision": "a44744751cd64ea5c0e8",
+    "url": "/RoboBuddies/static/js/2.5ea1078f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RoboBuddies/static/js/2.5ea1078f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1554304c156b50bd5ba",
+    "url": "/RoboBuddies/static/js/main.29a833cb.chunk.js"
+  },
+  {
+    "revision": "98f608b42c338e2817fc",
+    "url": "/RoboBuddies/static/js/runtime-main.bd62d40d.js"
+  }
+]);
